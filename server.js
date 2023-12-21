69c3786cb8ad54e45539f16f0b682d372450d274
Ho Chi An <hochian31@gmail.com>
2023-12-21 22:24:13 +0700

    first commit

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+// const port = 3000;
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
